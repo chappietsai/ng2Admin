f0187a4452815e8747676d8c02808d17ee035c4c
chappie <aboutskygooogle@gmail.com>
2017-12-15 16:00:29 +0800

    intial

---
+++ src/app/pages/forms/forms.routing.ts
@@ -0,0 +1,21 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { Forms } from './forms.component';
+import { Inputs } from './components/inputs/inputs.component';
+import { Layouts } from './components/layouts/layouts.component';
+import { FormioPage } from './components/formio/formio.component';
+
+// noinspection TypeScriptValidateTypes
+const routes: Routes = [
+  {
+    path: '',
+    component: Forms,
+    children: [
+      { path: 'inputs', component: Inputs },
+      { path: 'layouts', component: Layouts },
+      { path: 'formio', component: FormioPage }
+    ]
+  }
+];
+
+export const routing = RouterModule.forChild(routes);
